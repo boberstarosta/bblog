60590a265648116aa59e90da31e3ed7c265972b8
Boberstarosta <boberstarosta@gmail.com>
2019-10-21 18:53:11 +0200

    added user profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
