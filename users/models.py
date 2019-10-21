8be51a36f0ee84f0692ac3d2e6df12bc99652efe
Boberstarosta <boberstarosta@gmail.com>
2019-10-21 21:16:41 +0200

    uploaded profile images are now resized

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from PIL import Image
 
 User = get_user_model()
 
@@ -10,3 +11,11 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            img.thumbnail((300, 300))
+            img.save(self.image.path)
